5d95bcfa322d47dbc61c52801021a8c4c8066592
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 16:24:15 +0530

    range slider vue component

---
+++ src/components/RangeSlider.vue
@@ -0,0 +1,39 @@
+<template>
+    <form v-on:input="trial">
+    <p class="range-field" v-bind:style="sliderStyle">
+        <input type="range" id="RangeSlider" value="0" min="0" max="100"  />
+    </p>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'RangeSlider',
+        props: ['sliderStyle'],
+        data: function() {
+            return {
+                inputSlider: null
+            }
+        },
+        mounted: function() {
+            this.$nextTick(function () {
+                let array_of_dom_elements = document.querySelectorAll("input[type=range]")
+                M.Range.init(array_of_dom_elements);
+            })
+            
+            this.inputSlider = this.$el[0] //set this.inputSlider to form's 'input' tag
+        },
+        methods: {
+            trial: function() {
+                this.$emit('sliderInput', this.inputSlider.value)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.range-label{
+    background-color: black;
+}
+</style>
+
