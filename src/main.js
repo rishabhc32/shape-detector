e84910f85bd2480e51b7101f0d15c96c78ae08c4
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 23:50:49 +0530

    added loading class names

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
+import * as tf from '@tensorflow/tfjs'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    mounted: async function() {
+        let model = await tf.loadModel('model/model.json')
+        Vue.prototype.$model = model
+        
+        let res = await fetch('model/class_names.txt')
+        let text = await res.text()
+        Vue.prototype.$classArray = text.split('\n')
+    }
 }).$mount('#app')
