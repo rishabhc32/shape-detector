1ee37f18ceb5f89486034fb94e6a932910250619
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 16:23:56 +0530

    clear button vue component

---
+++ src/components/ClearButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <a class="waves-effect waves-light btn" v-on:click="handleClick">Clear</a>
+</template>
+
+<script>
+export default {
+    name: 'ClearButton',
+    methods: {
+        handleClick: function() {
+            this.$emit('ClearButtonClick')
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    display: block;
+    margin: auto;
+    width: 100px;
+}
+</style>
+
+
