0dfb41fe6a065ed0cc522f76d8b010aef93fed2f
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 18:02:54 +0530

    added prediction logic

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -6,24 +6,29 @@
             v-bind:sliderStyle="sliderStyle"
             v-on:sliderInput="setSliderValue" >
             </range-slider>
-            <clear-button v-on:ClearButtonClick="clearCanvas" />
+            <div class="predict-clear-buttons">
+                <custom-button v-on:ButtonClick="predictImage" buttonText="Predict" class="predict-button"/>
+                <custom-button v-on:ButtonClick="clearCanvas" buttonText="Clear"/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import RangeSlider from './RangeSlider'
-import ClearButton from './ClearButton'
+import CustomButton from './CustomButton'
+import * as tf from '@tensorflow/tfjs'
 
 export default {
     name: 'Canvas',
     components: {
         RangeSlider,
-        ClearButton
+        CustomButton
     },
     data: function() {
         return {
             fabricCanvas: null,
+            canvasContext: null,
             sliderStyle: {
                 width: '300px',
                 margin: 'auto',
@@ -31,6 +36,8 @@ export default {
         }
     },
     mounted: function() {
+        this.canvasContext = document.getElementById('fabric-canvas').getContext('2d')
+
         this.fabricCanvas = new fabric.Canvas('fabric-canvas', {isDrawingMode: true})
         this.fabricCanvas.freeDrawingBrush.color = 'blue'
     },
@@ -41,6 +48,41 @@ export default {
         },
         clearCanvas: function() {
             this.fabricCanvas.clear()
+        },
+        predictImage: function() {
+            let image = this.canvasContext.getImageData(0, 0, this.fabricCanvas.getWidth(), this.fabricCanvas.getHeight())
+            let pred = this.$model.predict(this.preprocessImage(image)).dataSync()
+
+            let maxIndex = this.maxIndex(pred)
+            alert(maxIndex + " " + pred[maxIndex])
+        },
+        preprocessImage: function(imgData) {
+            return tf.tidy(()=>{
+                let tensor = tf.fromPixels(imgData, 1)
+
+                const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
+                const offset = tf.scalar(255.0);
+                const normalized = tf.scalar(1.0).sub(resized.div(offset));
+                const batched = normalized.expandDims(0)
+
+                return batched
+            })
+        },
+        maxIndex: function(arr) {
+            if (arr.length === 0) {
+                return -1;
+            }
+
+            let max = arr[0], maxIndex = 0;
+
+            for (let i = 1; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    maxIndex = i;
+                    max = arr[i];
+                }
+            }
+
+            return maxIndex;
         }
     }
 }
@@ -52,5 +94,12 @@ export default {
 }
 canvas {
     border: 2px solid #26a69a;
+}
+.predict-clear-buttons {
+    width: 100%;
+    text-align: center;
+}
+.predict-button {
+    margin-right: 10px;
 }
 </style>
