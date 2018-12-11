03287d92a522ef91b6d5f0a3cab1a04e29516dee
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 16:23:02 +0530

    chnaged HelloWorld.vue to title.vue

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row s12">
+        <div class="card grey lighten-4">
+            <div class="card-content black-text">
+                <span class="brand-logo">Shape Detector</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: ['title']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.brand-logo {
+    font-size: 2.5rem;
+}
+.card {
+    margin-top: 0px;
+}
+</style>
