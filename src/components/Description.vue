3d28b26b5eab0c7e8b805a2c1df7a98bd40d5d86
rishabhc32 <rishabhc32@gmail.com>
2018-12-11 16:24:06 +0530

    description vue component

---
+++ src/components/Description.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col s12">
+        <p>
+            Shape Detector is a simple tool that uses CNN to recognize drawings. The CNN was trained to recognize 100 classes using the quick draw dataset. The accuracy is around 93%, it can be imporved but Google Colab has its limits.
+        </p>
+        <p>
+            Draw the image and see the predictions. The table shows top 5 predictions.
+        </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Description',
+}
+</script>
+
+<style scoped>
+p {
+   padding-left: 20px;
+   padding-right: 20px;
+   font-size: 1.20rem; 
+}
+</style>
